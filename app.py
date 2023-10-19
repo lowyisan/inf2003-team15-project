90686690a8170162424a7ad80c7f4dc20f7a0447
Aaron Lam <aaronlammpn@gmail.com>
2023-10-20 00:32:13 +0800

    Added connection management in app.py
    SSH status:
    Check if SSH is active before first request
    
    DB connection:
    - Create and open DB connection via SSH tunnel
    - Connection remains open until entire request context is done
    -Connection closed using teardown

--- app.py
+++ app.py
@@ -1,11 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import pymysql
+from sshtunnel import SSHTunnelForwarder
 
 app = Flask(__name__, template_folder="templates")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+# MySQL configuration
+db_config = {
+    'user': 'root',
+    'password': 'iLoveRoots99',
+    'db': 'inf2003projectdb',
+}
+
+# SSH Tunnel Setup
+def create_ssh_tunnel():
+    server = SSHTunnelForwarder(
+        '35.212.167.35',
+        ssh_username='dev',
+        ssh_password='iLoveDonuts99',
+        ssh_port=22,
+        remote_bind_address=('127.0.0.1', 3306)
+    )
+    server.start()
+    return server
+
+# Create the SSH tunnel when the application starts
+ssh_server = create_ssh_tunnel()
+
+# Function for creating DB Connection
+def get_db():
+    if not hasattr(g, 'db_connection'):
+        g.db_connection = pymysql.connect(
+            host='127.0.0.1',
+            port=ssh_server.local_bind_port,
+            **db_config
+        )
+    return g.db_connection
+
+# Before the first request, check the SSH tunnel
+@app.before_request
+def check_ssh_tunnel():
+    if not ssh_server.is_active:
+        print("SSH tunnel failed to establish.")
+        # You could raise an exception here or handle it as appropriate
+    else:
+        print("SSH tunnel is active")
+
+# Landing Page
 @app.route('/')
-def index():
-    return render_template("index.html")
+def index(): 
+    return render_template("index.html") 
+
+# Registraton Page
+@app.route('/register.html')
+def register():
+    # Create a DB connection
+    connection = get_db()
+    cursor = connection.cursor()
+
+    # Example query
+    cursor.execute("SELECT * FROM Users")
+    data = cursor.fetchall()
+
+    # Close the cursor and the connection
+    cursor.close()
+
+    return render_template("register.html", data=data)
 
 @app.route('/about.html')
 def about():
@@ -39,8 +99,12 @@ def error404():
 def appointment():
     return render_template("appointment.html")
 
-
-
+# Teardown function to close the database connection
+@app.teardown_appcontext
+def close_db_connection(exception):
+    db_connection = getattr(g, 'db_connection', None)
+    if db_connection is not None:
+        db_connection.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
